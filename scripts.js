b28e3edae3b072bca151722a520c64f60d1af0e3
MKMaleka <maxwellkagiso@gmail.com>
2024-10-27 21:08:55 +0200

    [MM] script update, fix the script to caluate correct and display errors in the console/browser

--- scripts.js
+++ scripts.js
@@ -5,5 +5,31 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // To check for empty inputs
+  if(!dividend || !divider){
+    result.innerText = "Division not performed. Both values are required in inputs. Try again."
+    return;
+  }
+  
+  // To check and validate if inputs are numeric
+  const numDividend = Number(dividend);
+  const numDivider = Number(divider);
+
+  // To actually check if inputs are values
+  if (isNaN(numDividend) || isNaN(numDivider)) {
+    document.body.innerText = "Non-numeric value entered. Please reload the page and try again";
+    console.error(new Error("Invalid input: Non-numeric value entered."));
+    return;
+  }
+
+  // Handle division by zero
+  if (numDivider === 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again.";
+    console.error(new Error("Division by zero error"));
+    return;
+  }
+
+  // Display whole number result without decimals
+  result.innerText = Math.floor(numDividend / numDivider);
 });
